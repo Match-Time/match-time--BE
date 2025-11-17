c915d7d6e1ff34b2ad1ca1d954afbdb6daae37be
ume24 <yoomee2009@naver.com>
2025-11-17 21:26:41 +0900

    Initial commit

---
+++ src/main/java/com/matchTime/MatchTimeApplication.java
@@ -0,0 +1,13 @@
+package com.matchTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matchTime/MatchTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
