ef288323d09b8996b1d8b99a7b7d7620fffe6821
유미 <134575309+ume24@users.noreply.github.com>
2025-11-22 03:05:54 +0900

    Update WebConfig.java

--- src/main/java/com/matchtime/config/WebConfig.java
+++ src/main/java/com/matchtime/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
